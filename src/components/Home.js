0091752c28bdaa7e50c30d9c8b57a96c27ba463e
gnoblin <gnoblin@gmail.com>
2018-07-31 04:43:58 +0300

    sd

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,27 +6,12 @@ import s from '../styles/home.style';
 
 export default function Home() {
   const repoReadmeLink = text => (
-    <Interactive
-      as="a"
-      {...s.link}
-      href="https://github.com/rafrex/spa-github-pages#readme"
-    >{text}</Interactive>
+   <div>text</div>
   );
 
   return (
     <div>
-      <p style={s.p}>
-        This is an example single page app built
-        with React and React&nbsp;Router using {' '}
-        <Code>BrowserRouter</Code>. Navigate with the links below and
-        refresh the page or copy/paste the url to test out the redirect
-        functionality deployed to overcome GitHub&nbsp;Pages incompatibility
-        with single page apps (like this one).
-      </p>
-      <p style={s.p}>
-        Please see the {repoReadmeLink('repo readme')} for instructions on how to
-        use this boilerplate to deploy your own single page app using GitHub Pages.
-      </p>
+     
       <div style={s.pageLinkContainer}>
         <Interactive
           as={Link}
