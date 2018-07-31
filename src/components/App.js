b50fdffa9d4147f19083bc6d4899d6e9e12faec8
gnoblin <gnoblin@gmail.com>
2018-07-31 04:55:33 +0300

    ыч

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import ExampleComponent from './ExampleComponent';
 import PageNotFound from './PageNotFound';
-import Breadcrumbs from './Breadcrumbs';
+
 import s from '../styles/app.style';
 
 export default function App() {
   return (
-    <div style={s.root}>
+    <div>
       
-      <h1 style={s.title}>SPA Routing WIP</h1>
+      <h1>SPA Routing WIP</h1>
 
-      <nav style={s.breadcrumbs}>
+      <nav>
         {/*<Breadcrumbs />*/}
       </nav>
 
